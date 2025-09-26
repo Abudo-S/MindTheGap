321587b8d2ff40f18191e893b3b53fec2299568e
Abudo-S <abudo.mohamed123456789@gmail.com>
2025-09-26 23:43:47 +0200

    Add adapted layer in AdaptedNSPTransformer.

--- AdaptedNSPTransformer.py
+++ AdaptedNSPTransformer.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from peft import PeftModel, LoraConfig, get_peft_model, TaskType
 import torch
 import torch.nn as nn
 from torch.optim import AdamW
@@ -10,35 +11,56 @@ import os
 LEARNING_RATE = 5e-5 #0.00005
 
 class AdaptedNSPTransformer(nn.Module):
-    def __init__(self, model_name="roberta-base", save_path=None, num_labels=2):
+    def __init__(self, model_name="roberta-base", use_adapter=False, num_labels=2):
         """
         Initializes the AdaptedNSPTransformer with a specified transformer model.
         Suggested model names are 'distilbert-base-uncased', 'albert-base-v2', 'roberta-base'.
-        nsp_dim: dimension of the feedforward layer in the classification head, it needs to be tuned.
-        It shall be reduced in case of overfitting, and increased in case of underfitting.
+        num_labels: the number of output labels for the classification task (2 for NSP) (3 for NLI).
         """
         super(AdaptedNSPTransformer, self).__init__()
         #self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
-        #init adapter layers
+        self.model_name = f"adapted_{model_name}" if use_adapter else model_name
+        self.use_adapter = use_adapter
 
-        #freeze pre-trained transformer parameters since we only want to train only the NSP head
-        #they won't be updated during backpropagation
-        for param in self.model.parameters():
-            param.requires_grad = False
+        #init adapter layer (LoRA)
+        self.peft_config = LoraConfig(
+            task_type=TaskType.SEQ_CLS, #for NSP
+            r=8,  #impacts the size of matrices (d.r) in the adapter layer
+            lora_alpha=16,
+            target_modules=["query", "value"], #attention layers to adapt
+            lora_dropout=0.1, #regularization
+        )
+
+        #combine the configured adapter with the main model
+        #note that adapted_model is just a wrapper that points to base model in the memory
+        self.adapted_model = get_peft_model(self.model, self.peft_config)
         
-        #unfreeze the classification head parameters to be trained
-        for param in self.model.classifier.parameters():
-            param.requires_grad = True
+        if self.use_adapter:
+            print("Trainable parameters in the NSP adapted model:")
+            self.adapted_model.print_trainable_parameters()
+        else:
+            #freeze pre-trained transformer parameters since we only want to train only the NSP head
+            #they won't be updated during backpropagation
+            for param in self.model.parameters():
+                param.requires_grad = False
+            
+            #unfreeze the classification head parameters to be trained
+            for param in self.model.classifier.parameters():
+                param.requires_grad = True
         
     def forward(self, input_ids, attention_mask):
         """
         token_type_ids are used for NSP to separate between tokens within two sentences.
-        [token_type_ids in not used in RoBERTa, Albert and DistilBERT models]
+        [token_type_ids are not used in RoBERTa, Albert and DistilBERT models]
         """
         #use adapter layers
+        outputs = None
+        if self.use_adapter:
+            outputs = self.adapted_model(input_ids=input_ids, attention_mask=attention_mask)
+        else:
+            outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
 
-        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
         return outputs
     
     def train_epoch(self, 
@@ -55,8 +77,6 @@ class AdaptedNSPTransformer(nn.Module):
         if device is None:
             device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
-        self.model.train()
-        self.model.to(device)
         train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
         # for batch_num, batch in tqdm(train_loader, len(train_loader)):
@@ -69,7 +89,16 @@ class AdaptedNSPTransformer(nn.Module):
             input_ids = input_ids.squeeze(1).to(device)
             attention_mask = attention_mask.squeeze(1).to(device)
 
-            outputs = self.model(input_ids=input_ids,attention_mask=attention_mask)
+            outputs = None
+            if self.use_adapter:
+                self.adapted_model.train()
+                self.adapted_model.to(device)
+                outputs = self.adapted_model(input_ids=input_ids,attention_mask=attention_mask)
+            else:
+                self.model.train()
+                self.model.to(device)
+                outputs = self.model(input_ids=input_ids,attention_mask=attention_mask)
+
             if hasattr(outputs, "logits"):
                 outputs = outputs.logits
             else:
@@ -98,16 +127,32 @@ class AdaptedNSPTransformer(nn.Module):
         #save the state_dict
         torch.save(classifier_state_dict, target_path)
 
-        print(f"Saved {self.model.__class__.__name__} classification layer to {target_path}")
+        print(f"Saved {self.model.__class__.__name__} classification layer in {target_path}")
     
     def load_nsp_layer(self, target_path="saved_models/nsp_classification_layer.pt"):
         assert os.path.exists(target_path), f"Error: The path '{target_path}' does not exist."
 
         #load the state_dict into the model's classification layer
         state_dict = torch.load(target_path)
-        self.model.classifier.load_state_dict(state_dict)
+        self.model.classifier.load_state_dict(state_dict, strict=True)
 
         print(f"Loaded {self.model.__class__.__name__} classification layer from {target_path}")
     
+    def save_adapter_layer(self, target_path="saved_models/nsp_adapter"):
+        assert os.path.exists(target_path), f"Error: The path '{target_path}' does not exist."
 
+        #save the adapter
+        self.adapted_model.save_pretrained(target_path)
+
+        print(f"Saved {self.model.__class__.__name__} adapter layer in {target_path}")
+    
+
+    def load_adapter_layer(self, target_path="saved_models/nsp_adapter"):
+        assert os.path.exists(target_path), f"Error: The path '{target_path}' does not exist."
+
+        #load the adapter with the base model
+        self.adapted_model = PeftModel.from_pretrained(self.model, target_path)
+
+        print(f"Loaded {self.model.__class__.__name__} adapter layer from {target_path}")
+    
 
